28cbaf186893304685c13609c41a52ec894a8264
Arunachalam Bharathy <arunachalam.b@thoughtworks.com>
2020-02-07 17:48:10 +0530

    [Add] Should return an available book

--- src/test/java/com/twu/biblioteca/LibraryTest.java
+++ src/test/java/com/twu/biblioteca/LibraryTest.java
@@ -2,8 +2,11 @@ package com.twu.biblioteca;
 
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 
 public class LibraryTest {
 
@@ -16,4 +19,13 @@ public class LibraryTest {
         assertEquals(0, books.size());
     }
 
+    @Test
+    void shouldReturnOneBookWhichIsInLibrary() {
+        Book book = mock(Book.class);
+        Library library = new Library(Collections.singletonList(book));
+
+        List<Book> books = library.getBooks();
+
+        assertEquals(1, books.size());
+    }
 }
