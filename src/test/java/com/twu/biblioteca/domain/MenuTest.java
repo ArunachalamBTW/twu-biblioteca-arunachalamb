20dff5be8ffc47bfde96f152baad92e66fec5637
Arunachalam Bharathy <arunachalam.b@thoughtworks.com>
2020-02-10 17:33:07 +0530

    [Add] Menu with exit option

--- src/test/java/com/twu/biblioteca/domain/MenuTest.java
+++ src/test/java/com/twu/biblioteca/domain/MenuTest.java
@@ -63,6 +63,20 @@ class MenuTest {
         System.setIn(sysInBackup);
     }
 
+    @Test
+    void shouldNotifyWhenInvalidOptionIsSelected() {
+        InputStream sysInBackup = System.in;
+        String input = "9" + NEW_LINE + "4";
+        ByteArrayInputStream input1 = new ByteArrayInputStream(input.getBytes());
+        System.setIn(input1);
+        Menu menu = new Menu(library);
+
+        menu.mainMenu();
+
+        assertEquals(MENU_OPTIONS + NEW_LINE + INVALID_OPTION + NEW_LINE + MENU_OPTIONS + NEW_LINE, consoleOutContent.toString());
+        System.setIn(sysInBackup);
+    }
+
     public String defaultBooksListString() {
         return "1. Programming Book 1" + BOOK_DETAILS_SEPARATORS + "2000" + BOOK_DETAILS_SEPARATORS + "Martin Fowler\n" +
                 "2. Programming Book 2" + BOOK_DETAILS_SEPARATORS + "2001" + BOOK_DETAILS_SEPARATORS + "Martin Fowler";
