07b0ca2793b3b265cea2e9d86ad265e5d875787a
Arunachalam Bharathy <arunachalam.b@thoughtworks.com>
2020-02-12 17:23:43 +0530

    [Refactor] Should not allow an user to login with wrong credentials

--- src/test/java/com/twu/biblioteca/controller/LoginTest.java
+++ src/test/java/com/twu/biblioteca/controller/LoginTest.java
@@ -61,6 +61,15 @@ class LoginTest {
         assertEquals(LOGIN_FAIL + NEW_LINE, consoleOutContent.toString());
     }
 
+    @Test
+    void shouldNotLoginAUserWithInvalidCredentials() {
+        Login login = new Login(getUsers(), Screen.getInstance());
+
+        login.login("123-4567", hellomd5 + "hello");
+
+        assertEquals(LOGIN_FAIL + NEW_LINE, consoleOutContent.toString());
+    }
+
     public List<User> getUsers() {
         User user1 = new User("Arun", "arun@abc.com", "9999999999", "123-4567", hellomd5);
         User user2 = new User("Bala", "bala@abc.com", "9999999999", "012-1234", hellomd5);
