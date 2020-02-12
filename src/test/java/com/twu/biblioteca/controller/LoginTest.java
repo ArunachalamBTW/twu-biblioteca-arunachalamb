5aa5c0eb4d413d59020a780830b9932b03f9faf9
Arunachalam Bharathy <arunachalam.b@thoughtworks.com>
2020-02-12 17:22:11 +0530

    [Refactor] Should not allow un registered user to login

--- src/test/java/com/twu/biblioteca/controller/LoginTest.java
+++ src/test/java/com/twu/biblioteca/controller/LoginTest.java
@@ -52,6 +52,15 @@ class LoginTest {
         assertEquals(LOGIN_SUCCESS + NEW_LINE, consoleOutContent.toString());
     }
 
+    @Test
+    void shouldNotLoginAUnRegisteredUser() {
+        Login login = new Login(getUsers(), Screen.getInstance());
+
+        login.login("123-45678", hellomd5);
+
+        assertEquals(LOGIN_FAIL + NEW_LINE, consoleOutContent.toString());
+    }
+
     public List<User> getUsers() {
         User user1 = new User("Arun", "arun@abc.com", "9999999999", "123-4567", hellomd5);
         User user2 = new User("Bala", "bala@abc.com", "9999999999", "012-1234", hellomd5);
